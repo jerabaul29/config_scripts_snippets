8db1d08ee4b8a13833663f4246f741396fe834fc
JRT <jean.rblt@gmail.com>
2021-10-13 22:07:16 +0200

    WIP

--- programming_languages/bash/preamble/example_preamble.sh
+++ programming_languages/bash/preamble/example_preamble.sh
@@ -7,9 +7,11 @@
 # when read only, remember to " declare -r SOME_VAR "
 # I like better to use some_function(), _SOME_VAR conventions
 # remember to check error codes: " $? "
+# rembmer to declare as much " local " variables as possible inside functions; possible to use any declare option with local, for example, " local -r SOME_VAR="bla" " is a local read only variable
 
 # sounder programming environment
 
+# for any of these " set " commands, use " +o " instead of " -o " to unset instead of set
 # exit if a command fails; to circumvent, can add specifically on commands that can fail safely: " || true "
 set -o errexit
 # make sure to show the error code of the first failing command
@@ -19,6 +21,7 @@ set -o noclobber
 # exit if try to use undefined variable
 set -o nounset
 # on globbing that has no match, return nothing, rather than return the dubious default ie the pattern itself
+# see " help shopt "; use the -u flag to unset (while -s is set)
 shopt -s nullglob
 
 # a few useful functions
@@ -40,4 +43,6 @@ cleanup() {
 # local variables are visible to called functions, so local is quite weak, but still use local as much as possible
 
 # PERFORM STATIC ANALYSIS on your scripts! https://github.com/koalaman/shellcheck
+
+# to see the flags that are set: echo "$-" (remember to look at " help set " to see which flags are which
 
--- scripts/z_grep_exec/zx.sh
+++ scripts/z_grep_exec/zx.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
+# exit if a command fails; to circumvent, can add specifically on commands that can fail safely: " || true "
+# set -o errexit
+# make sure to show the error code of the first failing command
+# set -o pipefail
+# do not overwrite files too easily
+# set -o noclobber
+# exit if try to use undefined variable
+# set -o nounset
+# on globbing that has no match, return nothing, rather than return the dubious default ie the pattern itself
+# shopt -s nullglob
+
 # in order to be able to do the change of the parent terminal, must be a function not a script
 function zx() {
     if [ $# -eq 0 ]; then
         echo "No arguments provided; -h for help"
-        return
+        # TODO: check error number
+        return 1
     fi 
 
     if [ "$1" == "-h" ]; then
         echo "Change path according to the result of zg"
         echo "use: zx number"
         echo "ex : zx 4"
-        return
+        return 0
+    fi
+    
+    if [ "$1" == "-h" ]; then
+        echo "Change path according to the result of zg v"
+        echo "use: zx number"
+        echo "ex : zx 4"
+        return 0
+    fi
+    
+    if [ "$1" == "-v" ]; then
+        echo "v2.0"
+        return 0
     fi
 
-    # TODO: automate how to set the PATH_TO_SAVELAST and share it with zx
-    PATH_TO_SAVELAST="/home/jrlab/Desktop/Git/MyBashScripts/Data/last_zg_output"
+    PATH_TO_SAVELAST="${HOME}/.last_zg_output"
 
     SELECTED_COMMAND="cd $(head -$1 ${PATH_TO_SAVELAST} | tail -1 | cut -c 7-)"
     echo "${SELECTED_COMMAND}"
     eval ${SELECTED_COMMAND}
+    
+    return 0
 }
+
