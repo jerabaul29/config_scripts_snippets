d042713d3e655b4cdef2f3ba05349a9686bf9ec4
JRT <jean.rblt@gmail.com>
2021-11-11 15:35:08 +0100

    Add example of icecream with no return value

--- programming_languages/python/icecream/icecream_preamble.py
+++ programming_languages/python/icecream/icecream_preamble.py
@@ -3,3 +3,11 @@ from icecream import ic
 # ------------------------------------------------------------------------------------------
 print("***** Configure icecream")
 ic.configureOutput(prefix='', outputFunction=print)
+
+meaning_of_life = 42
+
+res = ic(meaning_of_life)
+ic(res)
+
+# for now the solution I know of for avoiding useless prints on return values in ipython3 is to use _ =
+_ = ic(meaning_of_life)
