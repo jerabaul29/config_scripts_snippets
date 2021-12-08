73eff38d65f52ea4e6c2eb888f19c84a22180e07
JRT <jean.rblt@gmail.com>
2021-12-08 11:06:38 +0100

    Add the better self-documenting f format

--- programming_languages/python/str_formatting/str_format.py
+++ programming_languages/python/str_formatting/str_format.py
@@ -11,3 +11,7 @@ print(format_way)
 # even finer :)
 f_str_way = f"some_int has value {some_int}"
 print(f_str_way)
+
+# even better for "debug" uses: show the variable name
+f_str_debug_way = f"{some_int = }"
+print(f_str_debug_way)
