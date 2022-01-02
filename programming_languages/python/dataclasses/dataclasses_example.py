d181b337d257623a00caf0dfcf0cc4402eebb7f2
jerabaul29 <jean.rblt@gmail.com>
2022-01-02 17:12:43 +0100

    Add post_init example to python dataclasses

--- programming_languages/python/dataclasses/dataclasses_example.py
+++ programming_languages/python/dataclasses/dataclasses_example.py
@@ -6,13 +6,32 @@ import datetime
 class Some_Class:
     some_int: int
     some_float: float
+    some_float_less_than_3: float
     some_datetime: datetime.datetime
+
+    def __post_init__(self):
+        """our class consistency checks to be performed after initialization."""
+        # this risks to not be executed in release mode...
+        # assert self.some_float_less_than_3 < 3, f"got {self.some_float_less_than_3} for some_float_less_than_3"
+        if self.some_float_less_than_3 > 3:
+            raise RuntimeError(f"got {self.some_float_less_than_3} for some_float_less_than_3"
+)
 
 
 some_class_instance = Some_Class(
         some_int = 1,
         some_float = 3.14,
+        some_float_less_than_3 = 2.4,
         some_datetime = datetime.datetime.fromtimestamp(42)
     )
     
 print(some_class_instance)
+
+some_class_instance_erroneous = Some_Class(
+        some_int = 1,
+        some_float = 3.14,
+        some_float_less_than_3 = 3.4,
+        some_datetime = datetime.datetime.fromtimestamp(42)
+    )
+    
+print(some_class_instance_erroneous)
