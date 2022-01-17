3dbfcead0ce1f95643bb1b74e62dbce123c103c4
JRT <jean.rblt@gmail.com>
2022-01-17 15:19:07 +0100

    Add const class variable

--- programming_languages/python/dataclasses/dataclasses_example.py
+++ programming_languages/python/dataclasses/dataclasses_example.py
@@ -1,18 +1,23 @@
 from dataclasses import dataclass
+from typing import ClassVar
+
 import datetime
 
 
 @dataclass
 class Some_Class:
+    some_const: ClassVar[int] = 20
     some_int: int
     some_float: float
     some_datetime: datetime.datetime
+    some_use_of_const: int
 
 
 some_class_instance = Some_Class(
         some_int = 1,
         some_float = 3.14,
-        some_datetime = datetime.datetime.fromtimestamp(42)
+        some_datetime = datetime.datetime.fromtimestamp(42),
+        some_use_of_const = Some_Class.some_const * 4
     )
-    
+
 print(some_class_instance)
