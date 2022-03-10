d2bda3488cb217ac095fd615c887403476581928
JR <jerabaul29@users.noreply.github.com>
2022-03-10 20:43:00 +0100

    Update example_preamble.sh

--- programming_languages/bash/preamble/example_preamble.sh
+++ programming_languages/bash/preamble/example_preamble.sh
@@ -22,6 +22,7 @@ set -o errexit
 # make sure to show the error code of the first failing command
 set -o pipefail
 # do not overwrite files too easily
+# to override the noclobber: >| instead of > only
 set -o noclobber
 # exit if try to use undefined variable
 set -o nounset
