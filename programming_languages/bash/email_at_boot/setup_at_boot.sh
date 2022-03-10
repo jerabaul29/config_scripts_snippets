084b618cc5b3cfe07ae8c23bfde47e4449096169
JR <jerabaul29@users.noreply.github.com>
2022-03-10 21:12:11 +0100

    Create setup_at_boot.sh

---
+++ programming_languages/bash/email_at_boot/setup_at_boot.sh
@@ -0,0 +1,29 @@
+# exit if a command fails; to circumvent, can add specifically on commands that can fail safely: " || true "
+set -o errexit
+# make sure to show the error code of the first failing command
+set -o pipefail
+# do not overwrite files too easily
+set -o noclobber
+# exit if try to use undefined variable
+set -o nounset
+# on globbing that has no match, return nothing, rather than return the dubious default ie the pattern itself
+# see " help shopt "; use the -u flag to unset (while -s is set)
+shopt -s nullglob
+
+# give time to start
+echo "sleep for a few seconds to let the RPi start..."
+sleep 1
+
+echo "setup reverse ssh tunnel"
+# set up the reverse ssh tunnel
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa__nextbox__ssh_forwarding_user_s22a
+ssh -NR 8080:localhost:22 ssh_forwarding_user_s22a@jjrfnextbox.dedyn.io -p 31483 -o PreferredAuthentications=publickey -v &
+
+echo "prepare email"
+# prepare the email
+bash prepare_email.sh
+
+echo "send email"
+# send the email
+bash send_email.sh &
