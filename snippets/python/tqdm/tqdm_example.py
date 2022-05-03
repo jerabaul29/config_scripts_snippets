2fb0f135f3df6feb3207644c2400df609b365e06
JRT <jean.rblt@gmail.com>
2022-05-03 12:34:05 +0200

    Add some more tqdm example

--- snippets/python/tqdm/tqdm_example.py
+++ snippets/python/tqdm/tqdm_example.py
@@ -2,3 +2,11 @@ from tqdm import tqdm
 
 for i in tqdm(range(10000)):
     pass
+
+dict_in = {}
+dict_in_size = 50
+for i in range(dict_in_size):
+    dict_in[i] = "blabla"
+
+for ind, elem in tqdm(enumerate(dict_in), total=dict_in_size):
+    pass
