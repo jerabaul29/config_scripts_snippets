2bb27b9da00b4b1a284c32c6c9483e13f8270a2e
JRT <jean.rblt@gmail.com>
2022-11-08 22:19:47 +0100

    Add copula jointplot

--- snippets/python/seaborn/copula_view/copula_view_seaborn.py
+++ snippets/python/seaborn/copula_view/copula_view_seaborn.py
@@ -12,77 +12,64 @@ ic.configureOutput(prefix='', outputFunction=print)
 
 
 ############################################################
-# utilities from https://github.com/stochasticresearch/copula-py/blob/master/ecdf.py
-
-from scipy.interpolate import interp1d
 
 
-def ecdf(x_i, npoints):
-    """ Generates an Empirical CDF using the indicator function.
+def self_probability_integral_transform(sample_array_in):
+    """Given the sample_array_in empirical samples, get
+    the probability integral transform applied on the
+    same samples.
 
-    Inputs:
-    x_i -- the input data set, should be a numpy array
-    npoints -- the number of desired points in the empirical CDF estimate
+    sample_array_in: numpy array, shape (n_samples,)"""
 
-    Outputs:
-    y -- the empirical CDF
-    """
-    # define the points over which we will generate the kernel density estimate
-    x = np.linspace(min(x_i), max(x_i), npoints)
-    n = float(x_i.size)
-    y = np.zeros(npoints)
+    assert len(sample_array_in.shape) == 1
 
-    for ii in np.arange(x.size):
-        idxs = np.where(x_i<=x[ii])
-        y[ii] = np.sum(idxs[0].size)/n
+    n_samples = len(sample_array_in)
 
-    return (x,y)
+    # argsort provides the indexes_sorting such that:
+    # sample_array_in[indexes_sorting] is sorted
+    # however, what we want to perform the PIT is the index each entry
+    # would have when the array gets sorted; this is obtained by applying
+    # the inverse permutation to the arange of indexes
+    indexes_sorting = np.argsort(sample_array_in)
+    inverse_indexes_sorting = np.empty_like(indexes_sorting)
+    inverse_indexes_sorting[indexes_sorting] = np.arange(n_samples)
+    pit_transformed = np.arange(1, n_samples+1, 1)[inverse_indexes_sorting] / n_samples
 
+    ic(np.min(pit_transformed))
+    ic(np.max(pit_transformed))
 
-def probability_integral_transform(X):
-    """
-    Takes a data array X of dimension [M], ie M samples, and converts it to a uniform
-    random variable using the probability integral transform, U = F(X)
-    """
-    M = X.shape[0]
-
-    # convert X to U by using the probability integral transform:  F(X) = U
-    U = np.empty(X.shape)
-
-    # estimate the empirical cdf
-    (xx, pp) = ecdf(X, M)
-    f = interp1d(xx, pp, kind="nearest")
-
-    # plug this RV sample into the empirical cdf to get uniform RV
-    u = f(X)
-
-    return u
+    return pit_transformed
 
 ############################################################
 
-mvnorm = stats.multivariate_normal(mean=[0, 0], cov=[[1., 0.5],
-                                                     [0.5, 1.]])
+mvnorm = stats.multivariate_normal(mean=[0, 0], cov=[[1., 0.7],
+                                                     [0.7, 1.]])
 x = mvnorm.rvs(1000)
 
-# vanilla plot
-sns.jointplot(x=x[:, 0], y=x[:, 1], kind='kde', stat_func=None)
+############################################################
+# vanilla jointplot by setting the x and y arrays explicitly
+# sns.jointplot(x=x[:, 0], y=x[:, 1], kind='hex')
 
-# more fancy plot with pandas
+############################################################
+# vanilla jointplot by setting the x and y arrays be pd name
 dataframe_2d = pd.DataFrame(data=x, columns=["x0", "x1"])
 
-sns.jointplot(data=dataframe_2d, x="x0", y="x1", kind="kde")
+sns.jointplot(data=dataframe_2d, x="x0", y="x1", kind="hex")
 
-# view as a copula
-# compute the data resampled so that marginals are uniform
+############################################################
+# view as a copula like PIT-transformed jointplot
+
+# compute the data PIT transformed so that marginals are uniform
 for crrt_column in ["x0", "x1"]:
     crrt_data = dataframe_2d.loc[:, crrt_column].to_numpy(dtype=np.float32)
-    crrt_data_uniformized = probability_integral_transform(crrt_data)
+    crrt_data_pit_transformed = self_probability_integral_transform(crrt_data)
     new_column_name = crrt_column + "_uniformized"
-    dataframe_2d[new_column_name] = crrt_data_uniformized
+    dataframe_2d[new_column_name] = crrt_data_pit_transformed
 
 ic(dataframe_2d)
 
-sns.jointplot(data=dataframe_2d, x="x0_uniformized", y="x1_uniformized", kind="kde")
+sns.jointplot(data=dataframe_2d, x="x0_uniformized", y="x1_uniformized", kind="hex", xlim=(0.0, 1.0), ylim=(0.0, 1.0))
 
+############################################################
 plt.show()
 
