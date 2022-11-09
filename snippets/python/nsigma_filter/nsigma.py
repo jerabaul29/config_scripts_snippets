16a62f73ed38a5e8f528dcc9b6eb9cbeb146d4b5
JRT <jean.rblt@gmail.com>
2022-11-09 14:00:03 +0100

    Add more nsigma filtering tools

--- snippets/python/nsigma_filter/nsigma.py
+++ snippets/python/nsigma_filter/nsigma.py
@@ -34,3 +34,58 @@ def sliding_filter_nsigma(np_array_in, nsigma=3.0, side_half_width=5, skip_aroun
             list_filtered_indexes.append(crrt_middle_index)
 
     return np_array, list_filtered_indexes
+
+
+def indexes_nsigma_outlier(np_array_in, n_sigma=5.0):
+    """Perform nsigma filtering of the 1D array np_array_in,
+    returning the boolean array indicating which points pass
+    the nsigma test.
+
+    IN:
+        np_array_in: numpy array, shape (n_samples,)
+        n_sigma: the threshold for rejecting data
+
+    OUT:
+        array_nsigma_validity: array of booleans indicating
+            for each point if it passed the nsigma test or not;
+            ie True indicates a valid point, False an outlier
+            according to the nsigma filter. shape (n_samples,)
+    """
+    sigma = np.std(np_array_in)
+    mean = np.mean(np_array_in)
+    array_nsigma_validity = (np.abs(np_array_in - mean) < n_sigma * sigma)
+
+    return array_nsigma_validity
+
+
+def indexes_both_valid(np_array_1, np_array_2, n_sigma=5.0):
+    """Perform joint nsigma filtering of np_array_1 and np_array_2, i.e.
+    return the array of indexes which pass the nsigma test for both
+    np_array_1 and np_array_2.
+
+    IN:
+        np_array_1: numpy array, shape (n_samples,)
+        np_array_2: numpy array, shape (n_samples,)
+        n_sigma: the threshold for rejecting data
+
+    OUT:
+        co_valid: array of booleans indicating
+            for each point if it passed the nsigma test or not;
+            on both np_array_1 and np_array_2 at this index;
+            ie True indicates a valid point at this index for
+            both np_array_1 and np_array_2, False an outlier
+            according to the nsigma filter in at least one of
+            them. shape (n_samples,)
+    """
+    valid_1 = indexes_nsigma_outlier(np_array_1, n_sigma=n_sigma)
+    valid_2 = indexes_nsigma_outlier(np_array_2, n_sigma=n_sigma)
+
+    # note: the logical_and reduce make it easy to add as many inputs as wanted
+    co_valid = np.logical_and.reduce(
+        (
+            valid_1,
+            valid_2,
+        )
+    )
+
+    return co_valid
