7d45f2ef27a500ab1ad95059ecf1550a48dbde5a
JRT <jean.rblt@gmail.com>
2022-11-18 13:25:48 +0100

    Add example numpy timestamp vs datetime

---
+++ snippets/python/numpy_timestamp_datetime/numpy_timestamp_datetime.py
@@ -0,0 +1,44 @@
+import time
+import os
+
+# ------------------------------------------------------------------------------------------
+print("***** Put the interpreter in UTC, to make sure no TZ issues")
+os.environ["TZ"] = "UTC"
+time.tzset()
+
+from icecream import ic
+
+import datetime
+import pytz
+
+import numpy as np
+
+utc_timezone = pytz.timezone("UTC")
+
+ic.configureOutput(prefix="", outputFunction=print)
+
+# np timestamp from string
+print("")
+print("--- np timestamp from string")
+utc_string = "2012-06-17T23:00:05.45300000Z"
+utc_np = np.datetime64(utc_string)
+ic(utc_string)
+ic(utc_np)
+
+# datetime to np timestamp
+print("")
+print("--- np timestamp from datetime")
+some_datetime = datetime.datetime.utcnow()
+some_datetime = utc_timezone.localize(some_datetime)
+np_datetime = np.datetime64(some_datetime)
+ic(some_datetime)
+ic(np_datetime)
+
+# np timestamp to datetime
+print("")
+print("--- datetime from np timestamp")
+ic(utc_np)
+datetime_from_np = datetime.datetime.utcfromtimestamp(int(utc_np)/1e9)
+datetime_from_np = utc_timezone.localize(datetime_from_np)
+ic(datetime_from_np)
+
