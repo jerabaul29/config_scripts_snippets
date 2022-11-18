8c234c2bbd85e808792547029708e3bbaa4a3cbb
JRT <jean.rblt@gmail.com>
2022-11-18 13:36:20 +0100

    wip

---
+++ snippets/python/pandas/pandas_show_full_line.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from icecream import ic
+
+ic.configureOutput(prefix="", outputFunction=print)
+
+data = np.array([[1.1, 1.2, 1.3], [2.1, 2.2, 2.3], [3.1, 3.2, 3.3]])
+some_pd = pd.DataFrame(data=data, columns=["some_column", "column_1", "column_2"])
+ic(some_pd)
+
+print(some_pd.loc[0, :].to_string())
+
