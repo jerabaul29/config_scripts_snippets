8d9b19d5015f2732bdb5d9c29c00d38cc818b77f
JR <jean.rblt@gmail.com>
2023-12-12 10:35:16 +0100

    Add better mat loading solution

--- snippets/python/load_matlab/load_matlab_mat_file/load_matlab_mat_file.py
+++ snippets/python/load_matlab/load_matlab_mat_file/load_matlab_mat_file.py
@@ -1,3 +1,6 @@
+# ---
+# method 1: often some issues
+
 import scipy.io
 from pathlib import Path
 
@@ -8,3 +11,10 @@ mat_data = scipy.io.loadmat(crrt_file)
 
 some_field_as_np = mat_data["field_name"].squeeze()
 
+# ---
+# method 2: has been working fine for me
+
+from pymatreader import read_mat
+
+data = read_mat("some_path")
+# from there, a well behaved dict is available :) 
