e94f1eb317cce7332d6d0734fc804648a10c1740
JR <jean.rblt@gmail.com>
2024-01-09 14:41:48 +0100

    Howto show all pd frame

--- snippets/python/pandas/pandas_show_full_line.py
+++ snippets/python/pandas/pandas_show_full_line.py
@@ -9,5 +9,10 @@ data = np.array([[1.1, 1.2, 1.3], [2.1, 2.2, 2.3], [3.1, 3.2, 3.3]])
 some_pd = pd.DataFrame(data=data, columns=["some_column", "column_1", "column_2"])
 ic(some_pd)
 
+# option 1: force to print as string; that may be a bit ugly in jupyter though...
 print(some_pd.loc[0, :].to_string())
+
+# another option: set the option
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
