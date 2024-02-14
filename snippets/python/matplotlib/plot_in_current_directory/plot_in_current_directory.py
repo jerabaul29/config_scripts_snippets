4e0092bf44caf95364a5d57eb65fede5d526c08c
JR <jean.rblt@gmail.com>
2024-02-14 09:44:50 +0100

    Add save into same directory example

---
+++ snippets/python/matplotlib/plot_in_current_directory/plot_in_current_directory.py
@@ -0,0 +1,5 @@
+# use the following preamble to plot in the current directory and save there too when using the figure save button
+
+import matplotlib as mpl
+import os
+mpl.rcParams["savefig.directory"] = ""
