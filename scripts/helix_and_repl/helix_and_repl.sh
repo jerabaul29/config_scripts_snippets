7558daf9f5de087fe20352eb771e7a9cf3cbd39d
JR <jerabaul29@users.noreply.github.com>
2024-07-24 15:06:30 +0200

    Update helix_and_repl.sh

--- scripts/helix_and_repl/helix_and_repl.sh
+++ scripts/helix_and_repl/helix_and_repl.sh
@@ -1,3 +1,13 @@
+# to work, this will require having a miniconda environment set up (with miniconda root in your home)
+# with env name myenv ; this is so that all repl / lsps / packages etc are available
+
+# I recommend to install as:
+# cd ~/Desktop/Git
+# git clone https://github.com/jerabaul29/config_scripts_snippets.git
+
+# I recommend to make available as the hj command by adding to your ~/.bashrc:
+# alias hj="~/Desktop/Git/config_scripts_snippets/scripts/helix_and_repl/helix_and_repl.sh"
+
 # exit if a command fails; to circumvent, can add specifically on commands that can fail safely: " || true "
 set -o errexit
 # make sure to show the error code of the first failing command
@@ -60,9 +70,11 @@ fi
 
 tmux split-window -h
 tmux split-window -v
-tmux send-keys -t 2 'eval "$(/home/jrmet/miniconda3/bin/conda shell.bash hook)" && conda activate myenv' C-m
-tmux send-keys -t 1 'eval "$(/home/jrmet/miniconda3/bin/conda shell.bash hook)" && conda activate myenv' C-m
-tmux send-keys -t 3 'eval "$(/home/jrmet/miniconda3/bin/conda shell.bash hook)" && conda activate myenv' C-m
+# NOTE: the following pane numbers are for tmux set up with https://github.com/gpakosz/.tmux.git , i.e. tmux pane numbering starting at 1
+# for 0-indexed tmux panes (that is usually the default), reduce the indexes below by 1, i.e. 1->0 2->1 etc
+tmux send-keys -t 2 'eval "$(~/miniconda3/bin/conda shell.bash hook)" && conda activate myenv' C-m
+tmux send-keys -t 1 'eval "$(~/miniconda3/bin/conda shell.bash hook)" && conda activate myenv' C-m
+tmux send-keys -t 3 'eval "$(~/miniconda3/bin/conda shell.bash hook)" && conda activate myenv' C-m
 tmux send-keys -t 2 "ipython3" C-m
 if [ $USE_FILE = true ]; then
   tmux send-keys -t 1 "hlx $FILENAME" C-m
