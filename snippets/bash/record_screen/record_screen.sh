c25d817f4b9d42f6f1cadbb8f75721ea8c659f0f
JR <jerabaul29@users.noreply.github.com>
2024-07-31 11:13:03 +0200

    Update record_screen.sh

--- snippets/bash/record_screen/record_screen.sh
+++ snippets/bash/record_screen/record_screen.sh
@@ -1,5 +1,7 @@
 # example of snippet to record screen and then convert to a more easy format
 
+# method 1: "true" record
+
 # perform the recording itself
 # record screen: ctrl alt maj r to start and stop
 # it is also possible to use the "PrintScreen" key and use the video recording option on modern ubuntu
@@ -8,3 +10,7 @@
 sInput='Screencast from 24. juni 2021 kl. 13.43 +0200.webm' 
 sOutput="$(basename "${sInput%.*}")";
 ffmpeg -i "${sInput}" -pix_fmt rgb8 "${sOutput}.gif" && gifsicle --optimize=3 --output "${sOutput}-optimized.gif" --resize-height 600 "${sOutput}.gif"
+
+# method 2: "just" terminal record
+
+# if just recording terminal, consider using asciinema
